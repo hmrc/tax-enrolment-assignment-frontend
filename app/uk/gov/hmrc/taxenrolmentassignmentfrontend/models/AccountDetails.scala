245213d1b751eb11181f49f7520aea668f904422
Christine Stokes <18244373+Chrissystk@users.noreply.github.com>
2022-03-10 14:02:24 +0000

    TEN-

--- app/uk/gov/hmrc/taxenrolmentassignmentfrontend/models/AccountDetails.scala
+++ app/uk/gov/hmrc/taxenrolmentassignmentfrontend/models/AccountDetails.scala
@@ -16,4 +16,72 @@
 
 package uk.gov.hmrc.taxenrolmentassignmentfrontend.models
 
-case class AccountDetails(userId: String, email: String, lastLoginDate: String)
+import java.time
+import java.time.format.{DateTimeFormatter, FormatStyle}
+import java.time.{Instant, ZoneId, ZonedDateTime}
+
+import play.api.libs.json.{Format, Json}
+
+case class MFADetails(factorName: String, factorValue: String)
+//{
+//  def this(additonalFactors: AdditonalFactors) =
+//    this(factorName = additonalFactors.factorType match {
+//      case "totp"  => "Authentication app"
+//      case "voice" => "Voice call"
+//      case _       => "Text message"
+//    }, factorValue = additonalFactors.factorType match {
+//      case "totp" => additonalFactors.name.getOrElse("")
+//      case _      => additonalFactors.phoneNumber.getOrElse("")
+//    })
+//}
+
+object MFADetails {
+  implicit val format: Format[MFADetails] = Json.format[MFADetails]
+}
+
+case class AccountDetails(userId: String,
+                          email: Option[String],
+                          lastLoginDate: String,
+                          mfaDetails: Seq[MFADetails])
+//{
+//  def this(usersGroupResponse: UsersGroupResponse) =
+//    this(
+//      userId = usersGroupResponse.obfuscatedUserId,
+//      email = usersGroupResponse.email,
+//      lastLoginDate =
+//        AccountDetails.formatDate(usersGroupResponse.lastAccessedTimestamp),
+//      mfaDetails = usersGroupResponse.additionalFactors.map {
+//        additionalFactor =>
+//          new MFADetails(additionalFactor)
+//      }
+//    )
+//}
+
+object AccountDetails {
+
+  def formatDate(date: String): String = {
+    val zonedDateTime = ZonedDateTime.parse(date)
+    val datetimeFormatter = {
+      DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)
+    }
+    if (isToday(zonedDateTime)) {
+      "Today"
+    } else if (isYesterday(zonedDateTime)) {
+      "Yesterday"
+    } else {
+      zonedDateTime.format(datetimeFormatter)
+    }
+  }
+
+  def isToday(date: ZonedDateTime): Boolean = {
+    val todaysDate = ZonedDateTime.now(ZoneId.of("UTC"))
+    todaysDate.getYear == date.getYear && todaysDate.getMonth == date.getMonth && todaysDate.getDayOfMonth == date.getDayOfMonth
+  }
+
+  def isYesterday(date: ZonedDateTime): Boolean = {
+    val yesterdaysDate = ZonedDateTime.now(ZoneId.of("UTC")).minusDays(1L)
+    yesterdaysDate.getYear == date.getYear && yesterdaysDate.getMonth == date.getMonth && yesterdaysDate.getDayOfMonth == date.getDayOfMonth
+  }
+
+  implicit val format: Format[AccountDetails] = Json.format[AccountDetails]
+}

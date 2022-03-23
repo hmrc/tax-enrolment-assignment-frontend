ec7f9baf9b582e54126b37d32c10b296120a0ed3
Tawanda Siyamachira <62283841+TSiyamachiraQA@users.noreply.github.com>
2022-03-23 14:38:51 +0000

    TEN-83 Selected the other ID page (#33)
    * Sign in again page
    
    * TEN-83: sign in again view and controller
    
    * TEN-83: Adding for dan to take a look at
    
    * TEN-83 amend sign in page tests
    
    * TEN-83 amend test description
    
    Co-authored-by: yourgithubusernameDavid-HMRC <97685197+David-HMRC@users.noreply.github.com>

--- test/uk/gov/hmrc/taxenrolmentassignmentfrontend/controllers/helpers/TestFixture.scala
+++ test/uk/gov/hmrc/taxenrolmentassignmentfrontend/controllers/helpers/TestFixture.scala
@@ -37,29 +37,15 @@ import uk.gov.hmrc.http.{HeaderCarrier, SessionKeys}
 import uk.gov.hmrc.http.cache.client.CacheMap
 import uk.gov.hmrc.service.TEAFResult
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.config.AppConfig
-import uk.gov.hmrc.taxenrolmentassignmentfrontend.connectors.IVConnector
-import uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers.auth.{
-  AuthAction,
-  RequestWithUserDetails
-}
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.connectors.{EACDConnector, IVConnector, TaxEnrolmentsConnector}
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers.SignOutController
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers.helpers.TestData.userDetailsWithPTEnrolment
-import uk.gov.hmrc.taxenrolmentassignmentfrontend.connectors.{
-  EACDConnector,
-  IVConnector,
-  TaxEnrolmentsConnector
-}
-import uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers.auth.{
-  AuthAction,
-  RequestWithUserDetails
-}
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers.auth.{AuthAction, RequestWithUserDetails}
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers.testOnly.TestOnlyController
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.errors.TaxEnrolmentAssignmentErrors
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.logging.EventLoggerService
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.repository.TEASessionCache
-import uk.gov.hmrc.taxenrolmentassignmentfrontend.views.html.{
-  LandingPage,
-  UnderConstructionView
-}
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.views.html.{LandingPage, UnderConstructionView}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -88,18 +74,14 @@ trait TestFixture
   lazy val landingPageView: LandingPage = inject[LandingPage]
   val mockAuthConnector: AuthConnector = mock[AuthConnector]
   val mockIVConnector: IVConnector = mock[IVConnector]
-  val mockTaxEnrolmentsConnector: TaxEnrolmentsConnector =
-    mock[TaxEnrolmentsConnector]
+  val mockTaxEnrolmentsConnector: TaxEnrolmentsConnector = mock[TaxEnrolmentsConnector]
   val mockEacdConnector: EACDConnector = mock[EACDConnector]
   val testBodyParser: BodyParsers.Default = mock[BodyParsers.Default]
   lazy val requestPath = "somePath"
 
   implicit lazy val fakeRequest: FakeRequest[AnyContentAsEmpty.type] =
     FakeRequest("", requestPath)
-      .withSession(
-        SessionKeys.sessionId -> "foo",
-        SessionKeys.authToken -> "token"
-      )
+      .withSession(SessionKeys.sessionId -> "foo", SessionKeys.authToken -> "token")
       .withCSRFToken
       .asInstanceOf[FakeRequest[AnyContentAsEmpty.type]]
   val testAppConfig: AppConfig = app.injector.instanceOf[AppConfig]
@@ -116,7 +98,10 @@ trait TestFixture
   lazy val mcc: MessagesControllerComponents =
     stubMessagesControllerComponents()
 
+  lazy val mockSignOutController = mock[SignOutController]
+
   def doc(result: Html): Document = Jsoup.parse(contentAsString(result))
+
 
   def createInboundResult[T](result: T): TEAFResult[T] =
     EitherT.right[TaxEnrolmentAssignmentErrors](Future.successful(result))

8617851ae749912ebca9e0b3f8546bf295bb86bc
Jay Parekh <jay.parekh@digital.hmrc.gov.uk>
2022-04-04 12:10:59 +0100

    created a route and contollers along with controller test

---
+++ app/uk/gov/hmrc/taxenrolmentassignmentfrontend/controllers/ReportSuspiciousIdController.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers
+
+import com.google.inject.{Inject, Singleton}
+import play.api.Logger
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.config.AppConfig
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers.auth.AuthAction
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.forms.EnrolCurrentUserIdForm
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.logging.EventLoggerService
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.logging.LoggingEvent._
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.models.{AccountDetails, MFADetails}
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.repository.SessionKeys.REDIRECT_URL
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.repository.TEASessionCache
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.services.{EACDService, UsersGroupSearchService}
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.views.html.{PTEnrolmentOnAnotherAccount, ReportSuspiciousID}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class ReportSuspiciousIdController @Inject()(
+                                              authAction: AuthAction,
+                                              mcc: MessagesControllerComponents,
+                                              logger: EventLoggerService,
+                                              reportSuspiciousId: ReportSuspiciousID
+                                            )(implicit ec: ExecutionContext, appConfig: AppConfig)
+  extends FrontendController(mcc)
+    with I18nSupport {
+
+  implicit val baseLogger: Logger = Logger(this.getClass.getName)
+
+  def view(): Action[AnyContent] = authAction.async { implicit request =>
+    val mfaDetails = Seq(
+      MFADetails("Text message", "07390328923")
+    )
+    val fixedAccountDetails = AccountDetails(
+      "********3214",
+      Some("email1@test.com"),
+      "Yesterday",
+      mfaDetails
+    )
+
+    Future.successful(
+      Ok(reportSuspiciousId(
+        fixedAccountDetails, true
+      ))
+    )
+  }
+}

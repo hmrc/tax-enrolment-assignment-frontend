cff5c40cd27694d41ab9e612d41079d4bd1f5bf3
Jay Parekh <jay.parekh@digital.hmrc.gov.uk>
2022-04-04 08:24:25 +0100

    added view and view specs related to new report suspicious page

---
+++ test/uk/gov/hmrc/taxenrolmentassignmentfrontend/controllers/messages/ReportSuspiciousIDMessages.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers.messages
+
+object ReportSuspiciousIDMessages {
+  val title = "Report a suspicious user ID"
+  val heading = "Report a suspicious user ID"
+  val paragraph1: String = "You will need to phone us so that we can give you access to your personal tax account. Please"
+  val paragraph2: String = "or make a note of it as this screen may time out after 15 minutes."
+  val linkParagraphText: String = "download a copy of this information to your device"
+  val button = "Continue"
+  val saPText = "You can continue to your personal tax account with your current user ID."
+
+  object hardCodedValues {
+    val userID = "*********8173"
+    val email = "test.email"
+    val lastSignIn = "17 October 2021 at 3:45pm"
+    val textMessage = "******17435"
+    val authAppName = "Google"
+  }
+
+  val telephone = Seq("Telephone:","0300 200 3600")
+  val outsideUK = Seq("Outside UK:","+44 161 930 8445")
+
+  val informationBlock = Seq(
+    "Information about opening hours, call charges and more",
+    "Opening hours are Monday to Friday: 8am to 6pm (closed weekends and bank holidays).",
+    "You can also use",
+    "if you cannot hear or speak on the phone: dial 18001 then 0300 200 3600.",
+    "Calls are charged at standard local rates but may be free to call depending on your phone tariff."
+  )
+
+  val detailBlockLink = "Relay UK"
+
+
+}
---
+++ test/uk/gov/hmrc/taxenrolmentassignmentfrontend/controllers/views/ReportSuspiciousIDSpec.scala
@@ -0,0 +1,199 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers.views
+
+import play.api.test.FakeRequest
+import play.twirl.api.HtmlFormat
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers.helpers.TestFixture
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers.messages.{ReportSuspiciousIDMessages}
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.views.html.ReportSuspiciousID
+
+class ReportSuspiciousIDSpec extends TestFixture {
+
+  val reportSuspiciousIdView: ReportSuspiciousID =
+    app.injector.instanceOf[ReportSuspiciousID]
+  val result: HtmlFormat.Appendable =
+    reportSuspiciousIdView()(FakeRequest(), testMessages)
+
+  val resultSA: HtmlFormat.Appendable =
+    reportSuspiciousIdView(true)(FakeRequest(), testMessages)
+
+  object Selectors {
+    val backLink = "govuk-back-link"
+    val heading = "govuk-heading-xl"
+    val body = "govuk-body"
+    val summaryListRow = "govuk-summary-list__row"
+    val summaryListKey = "govuk-summary-list__key"
+    val summaryListValue = "govuk-summary-list__value"
+    val links = "govuk-link"
+  }
+
+  "The Report suspicious ID Page" should {
+    "have a back link" that {
+      val backLink =
+        doc(result).getElementsByClass(Selectors.backLink)
+      "has the text back" in {
+        backLink.text() shouldBe "Back"
+      }
+    }
+    "contains the correct title" in {
+      doc(result).title shouldBe ReportSuspiciousIDMessages.title
+    }
+    "contains the correct heading" in {
+      doc(result).getElementsByClass(Selectors.heading)
+        .text() shouldBe ReportSuspiciousIDMessages.heading
+    }
+    "contains a suspicious userId summary details" that {
+      val suspiciousIdDetailsRows = doc(result).getElementsByClass(Selectors.summaryListRow)
+      "includes the userId field with correct value" in {
+        suspiciousIdDetailsRows
+          .get(0)
+          .getElementsByClass(Selectors.summaryListKey)
+          .text() shouldBe "User ID"
+        suspiciousIdDetailsRows
+          .get(0)
+          .getElementsByClass(Selectors.summaryListValue)
+          .text() shouldBe ReportSuspiciousIDMessages.hardCodedValues.userID
+      }
+      "includes the email field with correct value" in {
+        suspiciousIdDetailsRows
+          .get(1)
+          .getElementsByClass(Selectors.summaryListKey)
+          .text() shouldBe "Email"
+        suspiciousIdDetailsRows
+          .get(1)
+          .getElementsByClass(Selectors.summaryListValue)
+          .text() shouldBe ReportSuspiciousIDMessages.hardCodedValues.email
+      }
+      "includes the last signed in field with correct value" in {
+        suspiciousIdDetailsRows
+          .get(2)
+          .getElementsByClass(Selectors.summaryListKey)
+          .text() shouldBe "Last signed in"
+        suspiciousIdDetailsRows
+          .get(2)
+          .getElementsByClass(Selectors.summaryListValue)
+          .text() shouldBe ReportSuspiciousIDMessages.hardCodedValues.lastSignIn
+      }
+      "includes the text message field with correct value" in {
+        suspiciousIdDetailsRows
+          .get(3)
+          .getElementsByClass(Selectors.summaryListKey)
+          .text() shouldBe "Text Message"
+        suspiciousIdDetailsRows
+          .get(3)
+          .getElementsByClass(Selectors.summaryListValue)
+          .text() shouldBe ReportSuspiciousIDMessages.hardCodedValues.textMessage
+      }
+      "includes the authenticator app field with correct value" in {
+        suspiciousIdDetailsRows
+          .get(4)
+          .getElementsByClass(Selectors.summaryListKey)
+          .text() shouldBe "Authenticator app"
+        suspiciousIdDetailsRows
+          .get(4)
+          .getElementsByClass(Selectors.summaryListValue)
+          .text() shouldBe ReportSuspiciousIDMessages.hardCodedValues.authAppName
+      }
+    }
+
+    "contains a valid paragraph for downloading the suspicious details along with download link" in {
+      val paragraph = doc(result).select("p." + Selectors.body)
+
+      paragraph.get(0)
+        .text() shouldBe ReportSuspiciousIDMessages.paragraph1
+
+      paragraph.get(1)
+        .text() shouldBe ReportSuspiciousIDMessages.paragraph2
+
+      doc(result)
+        .select("a." + Selectors.links)
+        .get(1)
+        .text() shouldBe ReportSuspiciousIDMessages.linkParagraphText
+    }
+
+    "contains the contact UK telephone details " in {
+      val telephoneBlock = doc(result).select("#telephone dt")
+
+      telephoneBlock.get(0)
+        .text() shouldBe ReportSuspiciousIDMessages.telephone(0)
+
+      telephoneBlock.get(1)
+        .text() shouldBe ReportSuspiciousIDMessages.telephone(1)
+    }
+
+    "contains the outside UK contact details " in {
+      val outsideUKBlock = doc(result).select("#outsideUk-telephone dt")
+
+      outsideUKBlock.get(0)
+        .text() shouldBe ReportSuspiciousIDMessages.outsideUK(0)
+
+      outsideUKBlock.get(1)
+        .text() shouldBe ReportSuspiciousIDMessages.outsideUK(1)
+    }
+
+    "contains the details block with valid details with in" that {
+      val detailsBlockParagraphs = doc(result)
+        .select("details p")
+
+      "correct title" in {
+        doc(result)
+          .select(".govuk-details__summary")
+          .text() shouldBe ReportSuspiciousIDMessages.informationBlock(0)
+      }
+      "correct information" in {
+        detailsBlockParagraphs
+          .get(0)
+          .text() shouldBe ReportSuspiciousIDMessages.informationBlock(1)
+        detailsBlockParagraphs
+          .get(1)
+          .text() shouldBe ReportSuspiciousIDMessages.informationBlock(2)
+        detailsBlockParagraphs
+          .get(2)
+          .text() shouldBe ReportSuspiciousIDMessages.informationBlock(3)
+        detailsBlockParagraphs
+          .get(3)
+          .text() shouldBe ReportSuspiciousIDMessages.informationBlock(4)
+        doc(result)
+          .select("details a")
+          .text() shouldBe ReportSuspiciousIDMessages.detailBlockLink
+      }
+    }
+
+    "should not display the continue button when no SA identified" in {
+      doc(result)
+        .select("div.sa > p")
+        .size() shouldBe 0
+
+      doc(result)
+        .select(".govuk-button")
+        .size() shouldBe 0
+    }
+
+    "should only display the continue button when SA identified" in {
+      doc(resultSA)
+        .select("div.sa > p." + Selectors.body)
+        .get(0)
+        .text() shouldBe ReportSuspiciousIDMessages.saPText
+
+      doc(resultSA)
+        .getElementsByClass("govuk-button").text shouldBe ReportSuspiciousIDMessages.button
+    }
+
+
+  }
+}

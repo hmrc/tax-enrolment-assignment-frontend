8c79dbdeaa1a1b69e7389d33c47187405363e23e
Tawanda Siyamachira <62283841+TSiyamachiraQA@users.noreply.github.com>
2022-05-10 15:51:14 +0100

    TEN-130 amend test data map to use random session Id

--- it/controllers/PTEnrolmentOnOtherAccountControllerISpec.scala
+++ it/controllers/PTEnrolmentOnOtherAccountControllerISpec.scala
@@ -453,6 +453,6 @@ class PTEnrolmentOnOtherAccountControllerISpec extends TestHelper with Status {
         UsersAssignedEnrolment(optSAEnrolledCredential)
       )
     )
-    await(save(sessionId, dataMap))
+    await(save(randomSessionId, dataMap))
   }
 }

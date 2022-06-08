ed4a43ab0c11643a90ac10e84a7c247f64065a47
Christine Stokes <18244373+Chrissystk@users.noreply.github.com>
2022-06-08 13:41:21 +0100

    TEN-189 adding endpoint for add taxes

--- app/uk/gov/hmrc/taxenrolmentassignmentfrontend/controllers/testOnly/TestOnlyController.scala
+++ app/uk/gov/hmrc/taxenrolmentassignmentfrontend/controllers/testOnly/TestOnlyController.scala
@@ -58,4 +58,9 @@ class TestOnlyController @Inject()(mcc: MessagesControllerComponents,
     implicit request =>
       Ok(Json.toJson(request.userDetails.enrolments.enrolments)(EnrolmentsFormats.writes).toString())
   }
+
+  def getSARedirectUrl(): Action[AnyContent] = Action.async{
+    implicit request =>
+
+  }
 }

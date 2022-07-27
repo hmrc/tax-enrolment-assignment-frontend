6ea8c3ca6cb6b0bfac532e47148f1845e571e60c
Tawanda Siyamachira <62283841+TSiyamachiraQA@users.noreply.github.com>
2022-07-27 11:36:41 +0100

    TEN-227 - Upgrade Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,16 +5,16 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"         %%  "bootstrap-frontend-play-28"  % "6.3.0",
-    "uk.gov.hmrc"         %%  "play-frontend-hmrc"          % "3.15.0-play-28",
+    "uk.gov.hmrc"         %%  "bootstrap-frontend-play-28"  % "6.4.0",
+    "uk.gov.hmrc"         %%  "play-frontend-hmrc"          % "3.22.0-play-28",
     "org.typelevel"       %%  "cats-core"                   % "2.7.0",
-    "uk.gov.hmrc"         %%  "domain"                      % "8.0.0-play-28",
+    "uk.gov.hmrc"         %%  "domain"                      % "8.1.0-play-28",
     "uk.gov.hmrc"         %%  "http-caching-client"         % "9.6.0-play-28",
-    "uk.gov.hmrc.mongo"   %%  "hmrc-mongo-play-28"          % "0.63.0"
+    "uk.gov.hmrc.mongo"   %%  "hmrc-mongo-play-28"          % "0.68.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %%  "bootstrap-test-play-28"    %  "6.3.0"  % Test,
+    "uk.gov.hmrc"             %%  "bootstrap-test-play-28"    %  "6.4.0"  % Test,
     "org.scalatest"           %%  "scalatest"                 %  "3.2.12"  % "test, it",
     "com.typesafe.play"       %%  "play-test"                 %  current   % Test,
     "org.scalatestplus.play"  %%  "scalatestplus-play"        %  "5.1.0"   % "test, it",

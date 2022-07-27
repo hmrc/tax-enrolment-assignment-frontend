c851bfcc6a8952232834eb5fa8161b6b46a65206
Tawanda Siyamachira <62283841+TSiyamachiraQA@users.noreply.github.com>
2022-07-27 12:38:09 +0100

    TEN-227 - Upgrade External Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"         %%  "bootstrap-frontend-play-28"  % "6.4.0",
     "uk.gov.hmrc"         %%  "play-frontend-hmrc"          % "3.22.0-play-28",
-    "org.typelevel"       %%  "cats-core"                   % "2.7.0",
+    "org.typelevel"       %%  "cats-core"                   % "2.8.0",
     "uk.gov.hmrc"         %%  "domain"                      % "8.1.0-play-28",
     "uk.gov.hmrc"         %%  "http-caching-client"         % "9.6.0-play-28",
     "uk.gov.hmrc.mongo"   %%  "hmrc-mongo-play-28"          % "0.68.0"
@@ -18,9 +18,9 @@ object AppDependencies {
     "org.scalatest"           %%  "scalatest"                 %  "3.2.12"  % "test, it",
     "com.typesafe.play"       %%  "play-test"                 %  current   % Test,
     "org.scalatestplus.play"  %%  "scalatestplus-play"        %  "5.1.0"   % "test, it",
-    "org.jsoup"               %   "jsoup"                     %  "1.14.3"  % "test, it",
+    "org.jsoup"               %   "jsoup"                     %  "1.15.2"  % "test, it",
     "com.vladsch.flexmark"    %   "flexmark-profile-pegdown"  %  "0.62.2"  % "test, it",
     "org.scalamock"           %%  "scalamock"                 %  "5.2.0"   % "test, it",
-    "com.github.tomakehurst"  %   "wiremock-jre8-standalone"  %  "2.33.1"  % "test, it"
+    "com.github.tomakehurst"  %   "wiremock-jre8-standalone"  %  "2.33.2"  % "test, it"
   )
 }

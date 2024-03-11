d102bb00df3b7c5f6229f9b93b0372d231c446d3
Nj147 <nathanjackson147@gmail.com>
2024-03-11 12:38:14 +0000

    DDCNL-8436 Test files and implementation of radio UI to add them done

--- app/uk/gov/hmrc/taxenrolmentassignmentfrontend/testOnly/controllers/TestOnlyController.scala
+++ app/uk/gov/hmrc/taxenrolmentassignmentfrontend/testOnly/controllers/TestOnlyController.scala
@@ -17,7 +17,7 @@
 package uk.gov.hmrc.taxenrolmentassignmentfrontend.testOnly.controllers
 
 import cats.implicits.toTraverseOps
-import play.api.libs.json.{JsArray, JsResultException}
+import play.api.libs.json.{JsArray, JsResultException, Json}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.http.{Authorization, HeaderCarrier}
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.controllers.actions.AuthJourney
@@ -27,11 +27,14 @@ import uk.gov.hmrc.taxenrolmentassignmentfrontend.logging.EventLoggerService
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.logging.LoggingEvent._
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.services.EACDService
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.testOnly.models.AccountDetailsTestOnly
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.testOnly.models.TestMocks
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.testOnly.utils.AccountUtilsTestOnly
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.views.html.testOnly.SelectTestData
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.testOnly.models.TestDataForm.selectUserForm
 
 @Singleton
 class TestOnlyController @Inject() (
@@ -39,9 +42,62 @@ class TestOnlyController @Inject() (
   mcc: MessagesControllerComponents,
   logger: EventLoggerService,
   authJourney: AuthJourney,
-  eacdService: EACDService
+  eacdService: EACDService,
+  selectTestDataPage: SelectTestData
 )(implicit ec: ExecutionContext)
     extends TEAFrontendController(mcc) {
+
+  def getTestDataInfo: Action[AnyContent] = Action { request =>
+    Ok(selectTestDataPage(TestMocks.mocks)(request, mcc.messagesApi.preferred(request)))
+  }
+
+  def insertTestData: Action[AnyContent] = Action.async { implicit request =>
+    implicit val hc: HeaderCarrier = HeaderCarrier(Some(Authorization("Bearer 1")))
+
+    selectUserForm
+      .bindFromRequest()
+      .fold(
+        _ =>
+          Future
+            .successful(BadRequest(selectTestDataPage(TestMocks.mocks)(request, mcc.messagesApi.preferred(request)))),
+        data => {
+          val fileName = data match {
+            case "Multiple Accounts: No Enrolments"                => "multipleAccountsNoEnrolments.json"
+            case "Multiple Accounts: One with PT and SA Enrolment" => "multipleAccountsOneWithPTAndSAEnrolment.json"
+            case "Multiple Accounts: One with PT Enrolment"        => "multipleAccountsOneWithPTEnrolment.json"
+            case "Multiple Accounts: One with PT Enrolment, another with SA Enrolment" =>
+              "multipleAccountsOneWithPTEnrolmentOtherWithSA.json"
+            case "Multiple Accounts: One with SA Enrolment"           => "multipleAccountsOneWithSAEnrolment.json"
+            case "Single User: No enrolments"                         => "singleUserNoEnrolments.json"
+            case "Single User: SA Enrolments"                         => "singleUserWithSAEnrolment.json"
+            case "Throttled Multiple Accounts: Has SA Enrolment"      => "throttledMultipleAccountsHasSA.json"
+            case "Throttled Multiple Accounts: No SA Enrolment"       => "throttledMultipleAccountsNoSA.json"
+            case "Throttled Multiple Accounts: One with PT Enrolment" => "throttledMultipleAccountsOneWithPT.json"
+          }
+          val x = Json.parse(FileHelper.loadFile(fileName))
+          println("aaaaaa " + x)
+          val y = Try(x.as[JsArray]) match {
+            case Success(_)                    => x.as[List[AccountDetailsTestOnly]]
+            case Failure(_: JsResultException) => List(x.as[AccountDetailsTestOnly])
+            case Failure(error)                => throw error
+          }
+          y.map { data =>
+            for {
+              _ <- accountUtilsTestOnly.deleteAccountDetails(data)
+              _ <- accountUtilsTestOnly.insertAccountDetails(data)
+            } yield ()
+          }.sequence
+            .fold(
+              {
+                case UpstreamError(error)              => InternalServerError(error.message)
+                case UpstreamUnexpected2XX(message, _) => InternalServerError(message)
+                case error                             => InternalServerError(error.toString)
+              },
+              _ => Ok("Created")
+            )
+        }
+      )
+  }
 
   def create: Action[AnyContent] = Action.async { request =>
     implicit val hc: HeaderCarrier = HeaderCarrier(Some(Authorization("Bearer 1")))
--- test/uk/gov/hmrc/taxenrolmentassignmentfrontend/testOnly/controllers/TestOnlyControllerSpec.scala
+++ test/uk/gov/hmrc/taxenrolmentassignmentfrontend/testOnly/controllers/TestOnlyControllerSpec.scala
@@ -19,7 +19,7 @@ package uk.gov.hmrc.taxenrolmentassignmentfrontend.testOnly.controllers
 import cats.data.EitherT
 import org.scalatest.matchers.must.Matchers._
 import play.api.Application
-import play.api.http.Status.OK
+import play.api.http.Status.{BAD_REQUEST, OK}
 import play.api.inject.bind
 import play.api.libs.json.Json
 import play.api.test.FakeRequest
@@ -27,7 +27,8 @@ import play.api.test.Helpers.{defaultAwaitTimeout, status}
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.errors.TaxEnrolmentAssignmentErrors
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.helpers.BaseSpec
-import uk.gov.hmrc.taxenrolmentassignmentfrontend.testOnly.models.AccountDetailsTestOnly
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.models.{AdditonalFactors, IdentifiersOrVerifiers}
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.testOnly.models.{AccountDetailsTestOnly, EnrolmentDetailsTestOnly, UserTestOnly}
 import uk.gov.hmrc.taxenrolmentassignmentfrontend.testOnly.utils.AccountUtilsTestOnly
 
 import scala.concurrent.Future
@@ -202,6 +203,139 @@ class TestOnlyControllerSpec extends BaseSpec {
       status(result) mustBe OK
     }
 
+  }
+
+  "postFormData" must {
+    "create an account with enrolmemts" in {
+      val nino = generateNino
+
+      val account = AccountDetailsTestOnly(
+        "98ADEA51-C0BA-497D-997E-F585FAADBCEH",
+        nino,
+        "Individual",
+        UserTestOnly("5217739547427626", "Firstname Surname", "email@example.com"),
+        List(
+          EnrolmentDetailsTestOnly(
+            "IR-SA",
+            IdentifiersOrVerifiers("UTR", "AA543004E"),
+            List(IdentifiersOrVerifiers("postcode", "postcode")),
+            "IR-SA Enrolment",
+            "Activated",
+            "principal"
+          )
+        ),
+        List(AdditonalFactors("totp", None, Some("HMRC-APP")))
+      )
+
+      val request = FakeRequest()
+        .withMethod("POST")
+        .withFormUrlEncodedBody(
+          "groupId"                             -> "98ADEA51-C0BA-497D-997E-F585FAADBCEH",
+          "nino.nino"                           -> nino.nino,
+          "affinityGroup"                       -> "Individual",
+          "user.credId"                         -> "5217739547427626",
+          "user.name"                           -> "Firstname Surname",
+          "user.email"                          -> "email@example.com",
+          "enrolments[0].serviceName"           -> "IR-SA",
+          "enrolments[0].identifiers.key"       -> "UTR",
+          "enrolments[0].identifiers.value"     -> "AA543004E",
+          "enrolments[0].verifiers[0].key"      -> "postcode",
+          "enrolments[0].verifiers[0].value"    -> "postcode",
+          "enrolments[0].enrolmentFriendlyName" -> "IR-SA Enrolment",
+          "enrolments[0].state"                 -> "Activated",
+          "enrolments[0].enrolmentType"         -> "principal",
+          "additionalFactors[0].factorType"     -> "totp",
+          "additionalFactors[0].name"           -> "HMRC-APP"
+        )
+
+      (mockAccountUtilsTestOnly
+        .deleteAccountDetails(_: AccountDetailsTestOnly)(_: HeaderCarrier))
+        .expects(account, *)
+        .returning(EitherT.rightT[Future, TaxEnrolmentAssignmentErrors](()))
+
+      (mockAccountUtilsTestOnly
+        .insertAccountDetails(_: AccountDetailsTestOnly)(_: HeaderCarrier))
+        .expects(account, *)
+        .returning(EitherT.rightT[Future, TaxEnrolmentAssignmentErrors](()))
+
+      val result = sut.postFormData.apply(request)
+
+      status(result) mustBe OK
+    }
+    "create an account with no enrolments" in {
+      val nino = generateNino
+
+      val account = AccountDetailsTestOnly(
+        "98ADEA51-C0BA-497D-997E-F585FAADBCEH",
+        nino,
+        "Individual",
+        UserTestOnly("5217739547427626", "Firstname Surname", "email@example.com"),
+        List(),
+        List(AdditonalFactors("totp", None, Some("HMRC-APP")))
+      )
+
+      val request = FakeRequest()
+        .withMethod("POST")
+        .withFormUrlEncodedBody(
+          "groupId"                             -> "98ADEA51-C0BA-497D-997E-F585FAADBCEH",
+          "nino.nino"                           -> nino.nino,
+          "affinityGroup"                       -> "Individual",
+          "user.credId"                         -> "5217739547427626",
+          "user.name"                           -> "Firstname Surname",
+          "user.email"                          -> "email@example.com",
+          "enrolments[0].serviceName"           -> "",
+          "enrolments[0].identifiers.key"       -> "",
+          "enrolments[0].identifiers.value"     -> "",
+          "enrolments[0].verifiers[0].key"      -> "",
+          "enrolments[0].verifiers[0].value"    -> "",
+          "enrolments[0].enrolmentFriendlyName" -> "",
+          "enrolments[0].state"                 -> "Activated",
+          "enrolments[0].enrolmentType"         -> "",
+          "additionalFactors[0].factorType"     -> "totp",
+          "additionalFactors[0].name"           -> "HMRC-APP"
+        )
+
+      (mockAccountUtilsTestOnly
+        .deleteAccountDetails(_: AccountDetailsTestOnly)(_: HeaderCarrier))
+        .expects(account, *)
+        .returning(EitherT.rightT[Future, TaxEnrolmentAssignmentErrors](()))
+
+      (mockAccountUtilsTestOnly
+        .insertAccountDetails(_: AccountDetailsTestOnly)(_: HeaderCarrier))
+        .expects(account, *)
+        .returning(EitherT.rightT[Future, TaxEnrolmentAssignmentErrors](()))
+
+      val result = sut.postFormData.apply(request)
+
+      status(result) mustBe OK
+    }
+    "create bad request when given invalid nino" in {
+
+      val request = FakeRequest()
+        .withMethod("POST")
+        .withFormUrlEncodedBody(
+          "groupId"                             -> "98ADEA51-C0BA-497D-997E-F585FAADBCEH",
+          "nino.nino"                           -> "",
+          "affinityGroup"                       -> "Individual",
+          "user.credId"                         -> "5217739547427626",
+          "user.name"                           -> "Firstname Surname",
+          "user.email"                          -> "email@example.com",
+          "enrolments[0].serviceName"           -> "",
+          "enrolments[0].identifiers.key"       -> "",
+          "enrolments[0].identifiers.value"     -> "",
+          "enrolments[0].verifiers[0].key"      -> "",
+          "enrolments[0].verifiers[0].value"    -> "",
+          "enrolments[0].enrolmentFriendlyName" -> "",
+          "enrolments[0].state"                 -> "Activated",
+          "enrolments[0].enrolmentType"         -> "",
+          "additionalFactors[0].factorType"     -> "totp",
+          "additionalFactors[0].name"           -> "HMRC-APP"
+        )
+
+      val result = sut.postFormData.apply(request)
+
+      status(result) mustBe BAD_REQUEST
+    }
   }
 
   "delete" must {

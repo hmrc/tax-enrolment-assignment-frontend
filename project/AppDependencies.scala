5f6b60bbc3356d37e93cc43682ddd75c5804eb6b
SandeepUmredkar <sandeep.umredkar@digital.hmrc.gov.uk>
2024-03-15 12:35:36 +0000

    DDCNL-8623

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,27 +2,28 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.15.0"
+  val bootstrapVersion = "8.5.0"
   val hmrcMongoVersion = "1.7.0"
-  val playVersion = "play-28"
+  val playVersion = "play-30"
 
   val compile = Seq(
     "uk.gov.hmrc"         %%  s"bootstrap-frontend-$playVersion"  % bootstrapVersion,
-    "uk.gov.hmrc"         %%  s"play-frontend-hmrc-$playVersion"  % s"8.5.0",
-    "org.typelevel"       %%  "cats-core"                   % "2.9.0",
-    "uk.gov.hmrc"         %%  "domain"                      % s"8.1.0-$playVersion",
-    "uk.gov.hmrc"         %%  "http-caching-client"         % s"10.0.0-$playVersion",
+    "uk.gov.hmrc"         %%  s"play-frontend-hmrc-$playVersion"  % "9.0.0",
+    "org.typelevel"       %%  "cats-core"                         % "2.10.0",
+    "uk.gov.hmrc"         %%  s"domain-$playVersion"              % "9.0.0",
+    "uk.gov.hmrc"         %%  s"http-caching-client-$playVersion" % "11.2.0",
     "uk.gov.hmrc.mongo"   %%  s"hmrc-mongo-$playVersion"          % hmrcMongoVersion,
-    "uk.gov.hmrc"         %%  s"crypto-json-$playVersion"         % "7.2.0"
+    "uk.gov.hmrc"         %%  s"crypto-json-$playVersion"         % "7.6.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %%  "bootstrap-test-play-28"    %  bootstrapVersion,
-    "org.mockito"             %% "mockito-scala-scalatest"       % "1.17.14",
-    "org.scalatest"           %%  "scalatest"                 %  "3.2.15",
-    "org.jsoup"               %   "jsoup"                     %  "1.15.4",
-    "com.vladsch.flexmark"    %   "flexmark-profile-pegdown"  %  "0.64.0",
+    "uk.gov.hmrc"             %%  s"bootstrap-test-$playVersion" %  bootstrapVersion,
+    "org.mockito"             %%  "mockito-scala-scalatest"       % "1.17.30",
+    "org.scalatest"           %%  "scalatest"                 %  "3.2.18",
+    "org.jsoup"               %   "jsoup"                     %  "1.17.2",
     "org.scalamock"           %%  "scalamock"                 %  "5.2.0",
-    "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion
-  ).map(_  % "test, it")
+    "uk.gov.hmrc.mongo"       %%  s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion
+  ).map(_  % "test")
+
+  val all: Seq[ModuleID] = compile ++ test
 }

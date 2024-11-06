651d28518d05b0ecd3e1c34b9d840dd0906430ac
Nj147 <nathanjackson147@gmail.com>
2024-11-06 14:16:27 +0000

    DDCNL-9390 Switched to use one-login-stub

---
+++ app/uk/gov/hmrc/taxenrolmentassignmentfrontend/testOnly/connectors/OneLoginStubConnectorTestOnly.scala
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.taxenrolmentassignmentfrontend.testOnly.connectors
+
+import cats.data.EitherT
+import play.api.Logging
+import play.api.http.Status.{CREATED, OK}
+import play.api.libs.json.JsObject
+import uk.gov.hmrc.http.HttpReads.Implicits._
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse, UpstreamErrorResponse}
+import uk.gov.hmrc.service.TEAFResult
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.errors.{UpstreamError, UpstreamUnexpected2XX}
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.testOnly.config.AppConfigTestOnly
+import uk.gov.hmrc.taxenrolmentassignmentfrontend.testOnly.models.{AccountDetailsTestOnly, Creds}
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class OneLoginStubConnectorTestOnly @Inject() (
+  httpClient: HttpClient,
+  appConfigTestOnly: AppConfigTestOnly
+)(implicit
+  ec: ExecutionContext
+) extends Logging {
+  def postAccount(account: AccountDetailsTestOnly)(implicit hc: HeaderCarrier): TEAFResult[String] = {
+    val url =
+      s"${appConfigTestOnly.oneLoginStubBaseUrl}/one-login-stub/test/accounts"
+
+    EitherT(
+      httpClient.POST[JsObject, Either[UpstreamErrorResponse, HttpResponse]](
+        url,
+        account.identityProviderAccountContextRequestBody
+      )
+    )
+      .transform {
+        case Right(response) if response.status == CREATED =>
+          Right(
+            (response.json \ "centralAuthUser" \ "_id").as[String]
+          )
+        case Right(response) =>
+          val ex = new RuntimeException(s"Unexpected ${response.status} status")
+          logger.error(ex.getMessage, ex)
+          Left(UpstreamUnexpected2XX(response.body, response.status))
+        case Left(upstreamError) =>
+          logger.error(upstreamError.message)
+          Left(UpstreamError(upstreamError))
+      }
+  }
+
+  def deleteAccount(caUserId: String)(implicit hc: HeaderCarrier): TEAFResult[Unit] = {
+    val url =
+      s"${appConfigTestOnly.oneLoginStubBaseUrl}/one-login-stub/test/accounts/$caUserId"
+
+    EitherT(
+      httpClient.DELETE[Either[UpstreamErrorResponse, HttpResponse]](
+        url
+      )
+    ).transform {
+      case Right(response) if response.status == OK => Right(())
+      case Right(response) =>
+        val ex = new RuntimeException(s"Unexpected ${response.status} status")
+        logger.error(ex.getMessage, ex)
+        Left(UpstreamUnexpected2XX(response.body, response.status))
+      case Left(upstreamError) =>
+        logger.error(upstreamError.message)
+        Left(UpstreamError(upstreamError))
+
+    }
+  }
+
+  def getAccount(caUserId: String)(implicit hc: HeaderCarrier): TEAFResult[List[String]] = {
+    val url =
+      s"${appConfigTestOnly.oneLoginStubBaseUrl}/one-login-stub/test/accounts/$caUserId"
+
+    EitherT(
+      httpClient.GET[Either[UpstreamErrorResponse, HttpResponse]](
+        url
+      )
+    ).transform {
+      case Right(response) =>
+        val listOfCreds: List[Creds] =
+          (response.json \ "credentials").as[List[Creds]]
+
+        Right(listOfCreds.map(_.caUserId))
+      case Left(_) =>
+        logger.warn(s"No account found for caUserId $caUserId")
+        Right(List.empty)
+    }
+  }
+
+}
--- app/uk/gov/hmrc/taxenrolmentassignmentfrontend/testOnly/utils/AccountUtilsTestOnly.scala
+++ app/uk/gov/hmrc/taxenrolmentassignmentfrontend/testOnly/utils/AccountUtilsTestOnly.scala
@@ -31,7 +31,8 @@ class AccountUtilsTestOnly @Inject() (
   enrolmentStoreServiceTestOnly: EnrolmentStoreServiceTestOnly,
   identityVerificationConnectorTestOnly: IdentityVerificationConnectorTestOnly,
   basStubsConnectorTestOnly: BasStubsConnectorTestOnly,
-  identityProviderAccountContextConnectorTestOnly: IdentityProviderAccountContextConnectorTestOnly
+  identityProviderAccountContextConnectorTestOnly: IdentityProviderAccountContextConnectorTestOnly,
+  oneLoginStubConnectorTestOnly: OneLoginStubConnectorTestOnly
 )(implicit ec: ExecutionContext) {
   def deleteAccountDetails(account: AccountDetailsTestOnly)(implicit hc: HeaderCarrier): TEAFResult[Unit] =
     for {
@@ -61,7 +62,7 @@ class AccountUtilsTestOnly @Inject() (
       for {
         // Insert enrolment-store data
         eacdIds <- identityProviderAccountContextConnectorTestOnly.getEacdIds(account.nino.nino)
-        _       <- eacdIds.map(id => identityProviderAccountContextConnectorTestOnly.deleteIndividual(id)).sequence
+        _       <- eacdIds.map(id => oneLoginStubConnectorTestOnly.deleteAccount(id)).sequence
         _       <- enrolmentStoreServiceTestOnly.insertAccount(account)
         _       <- account.enrolments.map(enrolment => enrolmentStoreServiceTestOnly.upsertEnrolment(enrolment)).sequence
         _ <-
@@ -70,7 +71,7 @@ class AccountUtilsTestOnly @Inject() (
               enrolmentStoreServiceTestOnly.addEnrolmentToGroup(account.groupId, account.user.credId, enrolment)
             )
             .sequence
-        caUserId <- identityProviderAccountContextConnectorTestOnly.postAccount(account)
+        caUserId <- oneLoginStubConnectorTestOnly.postAccount(account)
         _        <- identityProviderAccountContextConnectorTestOnly.postIndividual(account, caUserId)
       } yield ()
     } else {
